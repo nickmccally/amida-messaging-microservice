bafd6efa87a55d1d3ad4931e17dd0d12337b120f
Christopher Best <bestchris@gmail.com>
2018-01-26 12:43:00 -0500

    add message to schema

--- graphql/schema.js
+++ graphql/schema.js
@@ -2,7 +2,14 @@ import {
     GraphQLSchema,
     GraphQLObjectType,
     GraphQLString,
+    GraphQLInt,
+    GraphQLList,
+    GraphQLBoolean,
 } from 'graphql';
+
+import db from '../config/sequelize';
+
+const Message = db.Message;
 
 export default new GraphQLSchema({
     query: new GraphQLObjectType({
@@ -12,6 +19,50 @@ export default new GraphQLSchema({
                 type: GraphQLString,
                 resolve() {
                     return 'as a horse!';
+                },
+            },
+            message: {
+                type: new GraphQLObjectType({
+                    name: 'Message',
+                    fields: {
+                        to: {
+                            type: new GraphQLList(GraphQLString),
+                        },
+                        from: {
+                            type: GraphQLString,
+                        },
+                        message: {
+                            type: GraphQLString,
+                        },
+                        subject: {
+                            type: GraphQLString,
+                        },
+                        owner: {
+                            type: GraphQLString,
+                        },
+                        originalMessageId: {
+                            type: GraphQLInt,
+                        },
+                        parentMessageId: {
+                            type: GraphQLInt,
+                        },
+                        readAt: {
+                            type: GraphQLString,
+                        },
+                        isDeleted: {
+                            type: GraphQLBoolean,
+                        },
+                        isArchived: {
+                            type: GraphQLBoolean,
+                        },
+                    },
+                }),
+                args: {
+                    id: { type: GraphQLInt },
+                },
+                resolve(parentObject, args) {
+                    const result = Message.findById(args.id);
+                    return result;
                 },
             },
         },
